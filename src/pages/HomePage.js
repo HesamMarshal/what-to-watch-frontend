21ad7157cc974d70b34147d21e3132631eeef483
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-11-23 12:51:22 +0330

    add home page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+// src/pages/HomePage.js
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to What to Watch</h1>
+      <a href="/trending">View Trending Movies</a>
+    </div>
+  );
+}
+
+export default HomePage;
