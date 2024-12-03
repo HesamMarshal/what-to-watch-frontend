afa74b7774ead3934d2d8cdc5796463ac2a673ea
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-12-03 10:44:41 +0330

    change route

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,7 +1,15 @@
 // src/services/movies.js
+import axios from "axios";
 import API from "./api";
 
 export const fetchTrendingMovies = async () => {
-  const response = await API.get("/movies/fetch-trending");
+  // const response = axios.get("https://backend.whattowatch.ir/movies/trending", {
+  //   withCredentials: false, // If cookies or authentication tokens are involved
+  // });
+
+  const response = await API.get(
+    "https://backend.whattowatch.ir/movies/trending"
+  );
+
   return response.data;
 };
